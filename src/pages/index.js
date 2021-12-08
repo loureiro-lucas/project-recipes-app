1af75c63da2f2f65311b98efbf5e4ce3de4d4b97
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-12-08 18:03:21 -0300

    create pages and fix routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,29 @@
+import Drinks from './Drinks';
+import Explore from './Explore';
+import ExploreDrinks from './ExploreDrinks';
+import ExploreFoods from './ExploreFoods';
+import Favorites from './Favorites';
+import Foods from './Foods';
 import Login from './Login';
+import Profile from './Profile';
+import RecipeDetails from './RecipeDetails';
+import RecipeIngredients from './RecipeIngredients';
+import RecipeInProgress from './RecipeInProgress';
+import RecipesByArea from './RecipesByArea';
+import RecipesDone from './RecipesDone';
 
-export default Login;
+export {
+  Drinks,
+  Explore,
+  ExploreDrinks,
+  ExploreFoods,
+  Favorites,
+  Foods,
+  Login,
+  Profile,
+  RecipeDetails,
+  RecipeIngredients,
+  RecipeInProgress,
+  RecipesByArea,
+  RecipesDone,
+};
