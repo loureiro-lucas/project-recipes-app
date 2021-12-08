2a5c85965136113f7a670bc9092707a4b33e8e04
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-12-08 17:24:00 -0300

    finish login page requirements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Routes from './routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './pages/Login';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
+      <Routes />
     </Switch>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,34 +1,52 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-function Login() {
+function Login({ history }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
 
-  const validateInputs = () => {
+  const validateEmail = (userEmail) => {
     // Consulta: https://ui.dev/validate-email-address-javascript/
-    const MIN_CHARACTER = 6;
     const EMAIL_REGEX = /\S+@\S+\.\S+/;
 
-    if (password.length >= MIN_CHARACTER && EMAIL_REGEX.test(email)) {
-      setIsButtonDisabled(false);
+    if (EMAIL_REGEX.test(userEmail)) {
+      setIsEmailValid(true);
     } else {
-      setIsButtonDisabled(true);
+      setIsEmailValid(false);
+    }
+  };
+
+  const validatePassword = (userPassword) => {
+    const MIN_CHARACTER = 6;
+
+    if (userPassword.length > MIN_CHARACTER) {
+      setIsPasswordValid(true);
+    } else {
+      setIsPasswordValid(false);
     }
   };
 
   const handleEmailInput = ({ target: { value } }) => {
     setEmail(value);
-    validateInputs();
+    validateEmail(value);
   };
 
   const handlePasswordInput = ({ target: { value } }) => {
     setPassword(value);
-    validateInputs();
+    validatePassword(value);
+  };
+
+  const handleSubmit = () => {
+    localStorage.setItem('mealsToken', '1');
+    localStorage.setItem('cocktailsToken', '1');
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/comidas');
   };
 
   return (
-    <form>
+    <form onSubmit={ handleSubmit }>
       <label htmlFor="email-input">
         Email:
         <input
@@ -57,12 +75,18 @@ function Login() {
         // redirect to /explorar
         type="submit"
         data-testid="login-submit-btn"
-        disabled={ isButtonDisabled }
+        disabled={ !(isEmailValid && isPasswordValid) }
       >
         Entrar
       </button>
     </form>
   );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
