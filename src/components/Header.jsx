d4c373289fe1b61372ecc400d773a0bb48ca3085
Gabriel Maia <maiagabrielc@gmail.com>
2022-01-04 19:54:23 -0300

    Initial changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 import RecipesContext from '../context/RecipesContext';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 import '../styles/Header.css';
 
 const Header = ({ pathname, showSearchIcon, pageTitle }) => {
-  const [isSearchBarShown, setIsSearchBarShown] = useState(false);
+  // const [isSearchBarShown, setIsSearchBarShown] = useState(false);
   const [searchValue, setSearchValue] = useState('');
   const [searchBy, setSearchBy] = useState('name');
 
-  const { filterRecipes } = useContext(RecipesContext);
+  const { filterRecipes,
+    isSearchBarShown, setIsSearchBarShown } = useContext(RecipesContext);
 
   const history = useHistory();
 
@@ -67,58 +69,12 @@ const Header = ({ pathname, showSearchIcon, pageTitle }) => {
       </div>
 
       { isSearchBarShown && (
-        <form
-          className="header-search-bar-container"
-          autoComplete="off"
-          onSubmit={ handleSubmitSearch }
-        >
-          <label htmlFor="search-input">
-            <input
-              type="text"
-              name="search-input"
-              className="search-input"
-              id="search-input"
-              value={ searchValue }
-              placeholder="Search recipe"
-              data-testid="search-input"
-              onChange={ handleSearchInput }
-            />
-          </label>
-          <div className="search-by-container" onChange={ handleSearchBy }>
-            <label htmlFor="ingredient">
-              <input
-                type="radio"
-                id="ingredient"
-                name="search-by"
-                value="ingredient"
-                data-testid="ingredient-search-radio"
-              />
-              Ingrediente
-            </label>
-            <label htmlFor="name">
-              <input
-                type="radio"
-                id="name"
-                name="search-by"
-                value="name"
-                defaultChecked
-                data-testid="name-search-radio"
-              />
-              Nome
-            </label>
-            <label htmlFor="first-letter">
-              <input
-                type="radio"
-                id="first-letter"
-                name="search-by"
-                value="first-letter"
-                data-testid="first-letter-search-radio"
-              />
-              Primeira Letra
-            </label>
-          </div>
-          <button type="submit" data-testid="exec-search-btn">Buscar</button>
-        </form>
+        <SearchBar
+          handleSubmitSearch={ handleSubmitSearch }
+          searchValue={ searchValue }
+          handleSearchInput={ handleSearchInput }
+          handleSearchBy={ handleSearchBy }
+        />
       ) }
     </header>
   );
