7118002575954418f7815062e977630167b6eeca
Gustavo Lima <lima_gus@outlook.com>
2022-01-05 16:18:02 -0300

    req 70 - 74 - redirect should work but it doesnt

--- src/pages/ExploreRecipes.js
+++ src/pages/ExploreRecipes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -8,9 +9,65 @@ function ExploreRecipes({ location: { pathname } }) {
     ? 'Explorar Comidas'
     : 'Explorar Bebidas';
 
+  function renderExploreFoods() {
+    return (
+      <>
+        <Link to="explorar/comidas/ingredientes">
+          <button
+            type="button"
+            data-testid="explore-by-ingredient"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+
+        <Link to="explorar/comidas/area">
+          <button
+            type="button"
+            data-testid="explore-by-area"
+          >
+            Por Local de Origem
+          </button>
+        </Link>
+
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </>
+    );
+  }
+
+  function renderExploreDrinks() {
+    return (
+      <>
+        <Link to="explorar/bebidas/ingredientes">
+          <button
+            type="button"
+            data-testid="explore-by-ingredient"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+
+        <Link to="explorar/bebidas/area">
+          <button
+            type="button"
+            data-testid="explore-by-area"
+          >
+            Me Surpreenda!
+          </button>
+        </Link>
+      </>
+    );
+  }
+
   return (
     <>
       <Header pageTitle={ pageTitle } showSearchIcon={ false } />
+      { pathname === '/explorar/comidas' ? renderExploreFoods() : renderExploreDrinks()}
       <Footer />
     </>
   );
